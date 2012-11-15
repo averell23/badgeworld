0f81c6dcac0a0a093cfd8f65da71cca9ad48367a
Daniel Hahn <dha@betterplace.org>
2012-11-15 21:37:24 +0100

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Badgeworld::Application.config.secret_token = '0b1a69c43d3141e477881f80572b52795f9ce8241d3f237a580d46f6a14761183da66dee2ecc08fe49ba965dbf819056e34a84ae23265de3bc16fc83787e9553'
